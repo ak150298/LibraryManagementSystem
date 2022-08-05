87b7ff0b6b99d605686a8b5f06b24a01d606cf1c
Ayush Kansal <ayushmails070@gmail.com>
2022-08-04 20:40:46 -0700

    Added Mapping between Entities

--- subscription-service/src/main/java/com/lms/subscription/entity/Subscription.java
+++ subscription-service/src/main/java/com/lms/subscription/entity/Subscription.java
@@ -3,12 +3,7 @@ package com.lms.subscription.entity;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
-
-import com.lms.book.entity.Book;
-import com.lms.user.entity.User;
 
 @Entity
 public class Subscription {
@@ -17,32 +12,62 @@ public class Subscription {
 	private String subscriptionId;
 	private String subscribedOn;
 	private String validTill;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private User user;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Book book;
+
 	public String getSubscriptionId() {
 		return subscriptionId;
 	}
+
 	public void setSubscriptionId(String subscriptionId) {
 		this.subscriptionId = subscriptionId;
 	}
+
 	public String getSubscribedOn() {
 		return subscribedOn;
 	}
+
 	public void setSubscribedOn(String subscribedOn) {
 		this.subscribedOn = subscribedOn;
 	}
+
 	public String getValidTill() {
 		return validTill;
 	}
+
 	public void setValidTill(String validTill) {
 		this.validTill = validTill;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Subscription [subscriptionId=").append(subscriptionId).append(", subscribedOn=")
-				.append(subscribedOn).append(", validTill=").append(validTill).append("]");
+				.append(subscribedOn).append(", validTill=").append(validTill).append(", user=").append(user)
+				.append(", book=").append(book).append("]");
 		return builder.toString();
 	}
 	
 	
-	
+		
 	}
